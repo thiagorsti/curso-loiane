82ee84e2ae2fa7b5fe572e514f075cf7f6dae2b7
Thiago Rodrigues <thiago.rodrigues@trt18.jus.br>
2017-06-02 15:33:03 -0300

    .

--- servicos/src/app/app.component.ts
+++ servicos/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { CursosService } from './cursos/cursos.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
