85a5498e90583255973d60b86b46bc5ef21aaba8
Thiago Rodrigues <sistemas.thiago@gmail.com>
2017-06-06 09:46:39 -0300

    .

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }    
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
