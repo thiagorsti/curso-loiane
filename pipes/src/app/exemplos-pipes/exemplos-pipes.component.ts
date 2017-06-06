d26caddad7b2b67f506b8c39b21f41e0ed406238
Thiago Rodrigues <sistemas.thiago@gmail.com>
2017-06-06 11:12:23 -0300

    .

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -17,6 +18,32 @@ export class ExemplosPipesComponent implements OnInit {
   };
 
   livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string;
+
+  addCurso(valor) {
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+  obterCursos() {
+    if (this.livros.length === 0 || this.filtro === undefined || this.filtro.trim() === '') {
+      return this.livros;
+    }
+    return this.livros.filter((v) => {
+      if (v.toLowerCase().indexOf(this.filtro.toLowerCase()) >= 0) {
+        return true;
+      }
+      return false;
+    })
+  }
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
+
+  valorAsync2 = Observable.interval(2000)
+    .map(valor => 'Valor assíncrono 2');
 
   constructor() { }
 
