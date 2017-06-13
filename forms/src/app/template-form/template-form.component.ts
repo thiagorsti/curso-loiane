03c458d7194122236a6da99ab6bba3a6bb6f576f
Thiago Rodrigues <sistemas.thiago@gmail.com>
2017-06-13 15:06:54 -0300

    .

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-template-form',
@@ -11,7 +14,7 @@ export class TemplateFormComponent implements OnInit {
     nome: null,
     email: null
   };
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
@@ -30,5 +33,56 @@ export class TemplateFormComponent implements OnInit {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo)
     }
+  }
+
+  consultaCEP(cep, form) {
+    cep = cep.replace(/\D/g, '');
+    if (cep != null) {
+      let validaCep = /^[0-9]{8}$/;
+      if (validaCep.test(cep)) {
+        this.resetaDadosForm(form);
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)          
+          .map(dados => dados.json())
+          .subscribe(dados => this.populaDadosForm(dados, form));
+      }
+    }
+  }
+
+  populaDadosForm(dados, formulario) {    
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.email,
+    //   endereco: {
+    //     cep: dados.cep,
+    //     rua: dados.logradouro,        
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // });
+    formulario.form.patchValue({
+       endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,        
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });    
+  }
+
+  resetaDadosForm(formulario) {
+    formulario.form.patchValue({
+       endereco: {        
+        rua: null,        
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });    
   }
 }
