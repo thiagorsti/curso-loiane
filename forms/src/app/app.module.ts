71a4e97323dda2937aef27c82583dc350fb16c34
Thiago Rodrigues <sistemas.thiago@gmail.com>
2017-06-13 10:22:07 -0300

    merged conflicts

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { TemplateFormModule } from './template-form/template-form.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,    
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    TemplateFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
